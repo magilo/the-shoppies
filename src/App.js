e0ef532d8ef9b69ddf8f4aaadb339edd7888eb9a
magilo <53962625+magilo@users.noreply.github.com>
2021-01-11 17:11:46 -0500

    add scaffolding files to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Titlebar, Nominees, Search, Results } from './components'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Titlebar />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Nominees />
+        <Search />
+        <Results />
         <p>
           Edit <code>src/App.js</code> and save to reload.
           GREAT
---
+++ src/components/titlebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+//stateless component
+const Titlebar = () => (
+  <div>
+    <h1>Shoppies</h1>
+    <h6>Movie awards for entrepreneurs</h6>
+  </div>
+)
+
+export default Titlebar
