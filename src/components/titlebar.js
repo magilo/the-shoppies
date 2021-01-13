c8cb6b09f1cf369c7858341453eddd96be092e6e
magilo <53962625+magilo@users.noreply.github.com>
2021-01-12 19:49:20 -0500

    move assets into src, add logo

--- src/components/titlebar.js
+++ src/components/titlebar.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import shoppieslogo from '../assets/shoppies-logo-474x139.png';
+
 //stateless component
 const Titlebar = () => (
   <div>
-    <h1>Shoppies</h1>
+    <img src={shoppieslogo} alt="shoppieslogo" />
     <h6>Movie awards for entrepreneurs</h6>
   </div>
 )
